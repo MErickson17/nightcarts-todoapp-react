00d76cc87f9cf235fbb03c0e398c67e519c0fcdb
Mackenzie Erickson <77119565+MErickson17@users.noreply.github.com>
2021-11-29 09:47:53 -0700

    added login/sign up authentication for github and google

--- libs/hooks/useAuth.js
+++ libs/hooks/useAuth.js
@@ -3,21 +3,26 @@ import {onAuthStateChanged} from 'firebase/auth'
 
 import {auth} from 'libs/firebase'
 
-
 function useAuth() {
+
     const [user, setUser] = useState(null)
 
-    useEffect(()=>{
-        onAuthStateChanged(auth, (clientCredential)=>{
+    useEffect(()=>{ //component mounts
+        const authChange = onAuthStateChanged(auth, (clientCredential)=>{
             if(clientCredential){
                 setUser(clientCredential)
             }else{
                 setUser(null)
             }
         })
-    })
-    
+
+        // component unmount
+        return ()=> authChange()
+    }, []) // the empty array means there is no state for 
+           // react to watch and it will just run useEffect once- rather than looping
+
     return user
 }
+
 
 export {useAuth}
